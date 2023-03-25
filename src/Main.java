9304d02e9d457da54a06c94a33cfcb51dfb432a6
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-03-25 11:39:47 +0100

    added Interface Comparable and implemented in Class Student

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     {
         Student firstStudnet = new Student("Paweł", "Ogonek", StudentCondition.Chory, 2000, 15.6, "Krakowska 15" );
         firstStudnet.print();
+        Student secondStudnet = new Student("Paweł", "Ogórek", StudentCondition.Chory, 2000, 15.6, "Krakowska 15" );
+        Student thirdStudnet = new Student("Paweł", "Ogonek", StudentCondition.Chory, 2000, 15.6, "Krakowska 15" );
+        firstStudnet.compare(secondStudnet);
+        firstStudnet.compare(thirdStudnet);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class Student implements Comparable{
     String firstName;
     String lastName;
     StudentCondition studentCondition;
@@ -25,5 +25,11 @@ public class Student {
     void print()
     {
         System.out.println(this);
+    }
+
+    @Override
+    public void compare(Student comparedStudent) {
+        if (this.lastName == comparedStudent.lastName) System.out.printf("%s \ni porównywany\n %s \nmają takie samo nazwisko\n".formatted(this.toString(), comparedStudent.toString()));
+        else System.out.printf("%s \ni porównywany\n %s \nnie mają takiego samego nazwiska\n".formatted(this.toString(), comparedStudent.toString()));
     }
 }
